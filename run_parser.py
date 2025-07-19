c74e7ee9eb697fba1ef19d94abcb42b2af396c38
sigitbrahmana <46827989+sigitbrahmana@users.noreply.github.com>
2025-07-20 06:28:56 +0700

    Update run_parser.py

--- run_parser.py
+++ run_parser.py
@@ -4,9 +4,9 @@ from modules import app_optlog, app_log_parser
 # Sembunyikan menu dan footer
 hide_streamlit_style = """
             <style>
-            #MainMenu {visibility: visible;}
+            #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            header {visibility: visible;}
+            header {visibility: hidden;}
             
             /* Sembunyikan badge GitHub/Fork di pojok kanan atas */
             a[href*="github.com"] {
@@ -24,7 +24,7 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.set_page_config(page_title="Log Tools", layout="wide")
-st.sidebar.title("🔧 Menu")
+# st.sidebar.title("🔧 Menu")
 
 # Menu navigasi
 # app_choice = st.sidebar.radio("Pilih Tools:", ["CR LOG Parser", "OPTLOG Parser"])
