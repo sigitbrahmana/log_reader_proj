3f3798530a5ea73004c1631eba0511801ee9dcc4
sigitbrahmana <46827989+sigitbrahmana@users.noreply.github.com>
2025-07-20 03:22:36 +0700

    Update run_parser.py

--- run_parser.py
+++ run_parser.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Log Tools", layout="wide")
 st.sidebar.title("🔧 Menu")
 
 # Menu navigasi
-app_choice = st.sidebar.radio("Pilih Aplikasi:", ["CR LOG Parser", "OPTLOG Parser"])
+app_choice = st.sidebar.radio("Pilih Tools:", ["CR LOG Parser", "OPTLOG Parser"])
 
 # Panggil halaman berdasarkan pilihan
 if app_choice == "CR LOG Parser":
